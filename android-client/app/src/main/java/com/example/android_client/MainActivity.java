bad5c45a876b49c3d8c2b34c953d4db966412f5a
Andras Gemes <gemesa@protonmail.com>
2025-11-13 14:48:49 +0100

    chore: fix lint error

--- android-client/app/src/main/java/com/example/android_client/MainActivity.java
+++ android-client/app/src/main/java/com/example/android_client/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android_client;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -11,7 +12,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
 
